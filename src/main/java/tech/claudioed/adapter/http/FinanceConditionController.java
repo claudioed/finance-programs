7588353f226389cae696a19f86399d19a7040496
claudioed <claudioed.oliveira@gmail.com>
2023-04-01 18:55:05 -0300

    add some stuff

--- src/main/java/tech/claudioed/adapter/http/FinanceConditionController.java
+++ src/main/java/tech/claudioed/adapter/http/FinanceConditionController.java
@@ -1,7 +1,6 @@
 package tech.claudioed.adapter.http;
 
 import io.smallrye.common.constraint.NotNull;
-import java.util.List;
 import javax.validation.Valid;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
@@ -10,7 +9,6 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import tech.claudioed.adapter.http.utils.FormatFinanceCondition;
 import tech.claudioed.domain.financecondition.services.FinanceConditionService;
-import tech.claudioed.port.inputs.FinanceProgramQuery;
 import tech.claudioed.port.inputs.financecondition.NewFinanceCondition;
 import tech.claudioed.port.outputs.financecondition.FinanceConditionData;
 
@@ -29,14 +27,6 @@ public class FinanceConditionController {
   public FinanceConditionData createFinanceCondition(@Valid @NotNull NewFinanceCondition request) {
     var fc = this.financeConditionService.newFinanceCondition(request);
     return FormatFinanceCondition.from(fc);
-  }
-
-  @POST
-  @Path("/_search")
-  @Produces(MediaType.APPLICATION_JSON)
-  @Consumes(MediaType.APPLICATION_JSON)
-  public List<FinanceConditionData> findFinanceCondition(@Valid @NotNull FinanceProgramQuery request) {
-    return this.financeConditionService.find(request).stream().map(FormatFinanceCondition::from).toList();
   }
 
 }
